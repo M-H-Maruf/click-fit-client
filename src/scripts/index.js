a347cf485b3c956c0eb5594b267f923b20d709d8
M-H-Maruf <maruf.nightwalker@gmail.com>
2023-12-12 06:45:30 +0600

    preloader added

---
+++ src/scripts/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    // preloader appearance disappearance
+    $('#preloader').delay(1000).fadeOut(500, function () {
+        $('#content').fadeIn(500);
+    });
+});
