82c4a3d8cae0dc949576118f888a8308f24116aa
M-H-Maruf <maruf.nightwalker@gmail.com>
2023-12-12 07:56:51 +0600

    data fetching from api using ajax added

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,4 +4,16 @@ $(document).ready(function () {
         $('#content').fadeIn(500);
     });
 
+    // ajax call to numbers
+    $.ajax({
+        url: 'http://numbersapi.com/1/30/date?json',
+        type: 'GET',
+        dataType: 'json',
+        success: (data) => {
+            $('#api-content').html(data.text).addClass('fade-in');
+        },
+        error: function (error) {
+            console.log('Error fetching API data:', error);
+        }
+    });
 });
